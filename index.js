99021938ae51996adf539dcb88281bfaeb771f6b
niikennayu <nikenayusalsabila2919@gmail.com>
2026-06-10 18:10:16 +0700

    Fix working directory in index.js for Hostinger

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-import('./server/src/server.js');
+import process from 'process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Change working directory to server so dotenv finds server/.env
+process.chdir(path.join(__dirname, 'server'));
+
+// Now load the actual server
+import('./src/server.js');
