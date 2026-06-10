d018ab304e661195d3054d405d8e3a79599f2232
niikennayu <nikenayusalsabila2919@gmail.com>
2026-06-10 18:33:01 +0700

    Fix import path in app.js

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const path = require('path');
 process.chdir(path.join(__dirname, 'server'));
 
 // Now dynamically import the actual server (which is an ES module)
-import('./src/server.js').catch(err => {
+import('./server/src/server.js').catch(err => {
     console.error("Failed to load server:", err);
 });
